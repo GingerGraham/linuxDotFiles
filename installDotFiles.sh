5edc1386ea4c33f60bccad9b3373daa0e2659652
Graham Watts <34165628+GingerGraham@users.noreply.github.com>
2023-11-17 17:18:50 +0000

    Updating copy script

--- installDotFiles.sh
+++ installDotFiles.sh
@@ -2,7 +2,7 @@
 # This script creates symlinks from ~/ to a dotfiles directory cloned from github
 # Github source is https://github.com/GingerGraham/linuxDotFiles
 
-VERSION=2.0.2
+VERSION=2.0.3
 OPT_STRING=":adf:hlo:"
 
 TASK="" # Task to perform
@@ -96,62 +96,77 @@ set_copy_file () {
 }
 
 list_files () {
-    echo "Listing available files in ${DIR}"
-    for FILE in $(find ${DIR} -type f -name ".*" -exec basename {} \;); do
-        echo "${FILE}"
-    done
-    exit 0
+	echo "Listing available files in ${DIR}"
+	for SUBDIR in $(find ${DIR} -type d); do
+		echo "Subdirectory: ${SUBDIR}"
+		for FILE in $(find ${SUBDIR} -type f -name ".*" -exec basename {} \;); do
+			echo "${FILE}"
+		done
+	done
+	exit 0
 }
 
 create_old_file_dir () {
-    if [ ! -d "${OLD_FILE_DIR}" ]; then
-        echo "Creating ${OLD_FILE_DIR} for existing dot files"
-        if [ "${DRY_RUN}" = true ]; then
-            echo "mkdir -p ${OLD_FILE_DIR}"
-        else
-            mkdir -p "${OLD_FILE_DIR}"
-        fi
-    fi
+	CURRENT_DATE=$(date +"%Y-%m-%d")
+	CURRENT_TIME=$(date +"%H-%M-%S")
+	SUBDIR="${OLD_FILE_DIR}/${CURRENT_DATE}/${CURRENT_TIME}"
+	
+	if [ ! -d "${SUBDIR}" ]; then
+		echo "Creating ${SUBDIR} for existing dot files"
+		if [ "${DRY_RUN}" = true ]; then
+			echo "mkdir -p ${SUBDIR}"
+		else
+			mkdir -p "${SUBDIR}"
+		fi
+	fi
+	
+	OLD_FILE_DIR="${SUBDIR}"
 }
 
 copy_all_files () {
-    echo "Ensuring ${OLD_FILE_DIR} exists and creating if it does not"
-    create_old_file_dir
-    echo "Copying all dot files"
-    for FILE in $(find ${DIR} -type f -name ".*" -exec basename {} \;); do
-        if [ $FILE != ".gitignore" ]; then
-            echo "Moving existing dotfiles from ~ to ${OLD_FILE_DIR}"
-            if [ "${DRY_RUN}" = true ]; then
-                echo "mv ~/${FILE} ${OLD_FILE_DIR}"
-            else
-                mv ~/"${FILE}" "${OLD_FILE_DIR}"
-            fi
-            echo "Creating symlink to ${FILE} in home directory."
-            if [ "${DRY_RUN}" = true ]; then
-                echo "ln -s ${DIR}/${FILE} ~/${FILE}"
-            else
-                ln -s "${DIR}"/"${FILE}" ~/"${FILE}"
-            fi
-        fi
-    done
+	echo "Ensuring ${OLD_FILE_DIR} exists and creating if it does not"
+	create_old_file_dir
+	echo "Copying all dot files"
+	
+	# Find the dot files in any subdirectory
+	for FILE in $(find ${DIR} -type f -name ".*" -exec basename {} \;); do
+		if [ $FILE != ".gitignore" ]; then
+			echo "Moving existing dotfiles from ~ to ${OLD_FILE_DIR}"
+			if [ "${DRY_RUN}" = true ]; then
+				echo "mv ~/${FILE} ${OLD_FILE_DIR}"
+			else
+				mv ~/"${FILE}" "${OLD_FILE_DIR}"
+			fi
+			echo "Creating symlink to ${FILE} in home directory."
+			if [ "${DRY_RUN}" = true ]; then
+				echo "ln -s ${DIR}/${FILE} ~/${FILE}"
+			else
+				ln -s "${DIR}"/"${FILE}" ~/"${FILE}"
+			fi
+		fi
+	done
 }
 
 copy_selected_file () {
-    echo "Ensuring ${OLD_FILE_DIR} exists and creating if it does not"
-    create_old_file_dir
-    echo "Copying ${COPY_FILE}"
-    echo "Moving existing ${COPY_FILE} from ~ to ${OLD_FILE_DIR} if it exists"
-    if [ "${DRY_RUN}" = true ]; then
-        echo "mv ~/${COPY_FILE} ${OLD_FILE_DIR}"
-    else
-        mv ~/"${COPY_FILE}" "${OLD_FILE_DIR}"
-    fi
-    echo "Creating symlink to ${COPY_FILE} in home directory."
-    if [ "${DRY_RUN}" = true ]; then
-        echo "ln -s ${DIR}/${COPY_FILE} ~/${COPY_FILE}"
-    else
-        ln -s "${DIR}"/"${COPY_FILE}" ~/"${COPY_FILE}"
-    fi
+	echo "Ensuring ${OLD_FILE_DIR} exists and creating if it does not"
+	create_old_file_dir
+	echo "Copying ${COPY_FILE}"
+	
+	# Find the file(s) in any subdirectory
+	for FILE in $(find ${DIR} -type f -name "${COPY_FILE}" -exec basename {} \;); do
+		echo "Moving existing ${FILE} from ~ to ${OLD_FILE_DIR} if it exists"
+		if [ "${DRY_RUN}" = true ]; then
+			echo "mv ~/${FILE} ${OLD_FILE_DIR}"
+		else
+			mv ~/"${FILE}" "${OLD_FILE_DIR}"
+		fi
+		echo "Creating symlink to ${FILE} in home directory."
+		if [ "${DRY_RUN}" = true ]; then
+			echo "ln -s ${DIR}/${FILE} ~/${FILE}"
+		else
+			ln -s "${DIR}"/"${FILE}" ~/"${FILE}"
+		fi
+	done
 }
 
 copy_theme_files () {
