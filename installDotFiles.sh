edd51c956b352f016cfe204cf265349cffbe2265
Graham Watts <34165628+GingerGraham@users.noreply.github.com>
2024-02-19 10:53:54 +0000

    Excluded Files & Dirs + Selected File Copy Fix
    - Allowed for excluding files and directories
    - Fixed selected file copy function
    - Removed unused copy themes

--- installDotFiles.sh
+++ installDotFiles.sh
@@ -2,7 +2,7 @@
 # This script creates symlinks from ${HOME}/ to a dotfiles directory cloned from github
 # Github source is https://github.com/GingerGraham/linuxDotFiles
 
-VERSION=2.1.1
+VERSION=2.2.1
 OPT_STRING=":adf:hlo:"
 
 TASK="" # Task to perform
@@ -10,11 +10,26 @@ DIR=$(pwd)
 OLD_FILE_DIR=${HOME}/.oldDotFiles
 COPY_FILE="" # File to copy
 DRY_RUN=false
+EXCLUDE_DIRS=("docs" ".git")
+EXCLUDE_FILES=(".gitignore" "installDotFiles.sh" "README.md")
+
+# Generate an EXCLUDED_DIRS_ARGS string to pass to the find command
+EXCLUDED_DIRS_ARGS=""
+for dir in "${EXCLUDE_DIRS[@]}"; do
+    # If dir is .git then exclude it and any subdirectories
+    EXCLUDED_DIRS_ARGS+=" -not -name ${dir}"
+done
+
+# Generate an EXCLUDED_FILES_ARGS string to pass to the find
+EXCLUDED_FILES_ARGS=""
+for file in "${EXCLUDE_FILES[@]}"; do
+    EXCLUDED_FILES_ARGS+=" -not -name ${file}"
+done
 
 main () {
     opts "${@}"
     if [[ -z ${TASK} ]]; then
-        echo "No task selected" >&2
+        echo "[ERROR] No task selected" >&2
         usage
         exit 1
     fi
@@ -30,7 +45,7 @@ opts () {
                 TASK=("copy_all_files")
                 ;;
             d)
-                echo "Dry run" >&2
+                echo "[WARN] Dry run selected - no files or links will be changed" >&2
                 DRY_RUN=true
                 ;;
             f)
@@ -48,12 +63,12 @@ opts () {
                 set_old_file_dir "${OPTARG}"
                 ;;
             \?)
-                echo "Invalid option: -${OPTARG}" >&2
+                echo "[ERROR] Invalid option: -${OPTARG}" >&2
                 usage
                 exit 1
                 ;;
             :)
-                echo "Option -${OPTARG} requires an argument." >&2
+                echo "[ERROR] Option -${OPTARG} requires an argument." >&2
                 usage
                 exit 1
                 ;;
@@ -87,22 +102,18 @@ set_old_file_dir () {
 }
 
 set_copy_file () {
-    if [ -f "${1}" ]; then
-        COPY_FILE="${1}"
-    else
-        echo "[ERROR] File ${1} does not exist, exiting"
+    # Ensure an argument is passed in and set the COPY_FILE variable or return an error
+    if [ -z "${1}" ]; then
+        echo "[ERROR] No file name passed in, exiting"
+        echo "[INFO] Use -l to list available files"
         exit 1
     fi
+    COPY_FILE="${1}"
 }
 
 list_files () {
     echo "[INFO] Listing available files in ${DIR}"
-    find ${DIR} -type d -not -path "*/.git/*" -not -name ".git" -not -name "docs" | while IFS= read -r SUBDIR; do
-        echo "[INFO] Subdirectory: ${SUBDIR}"
-        find ${SUBDIR} -type f -name ".*" -exec basename {} \; | while IFS= read -r FILE; do
-            echo "${FILE}"
-        done
-    done
+    find ${DIR} -type f -name ".*" ${EXCLUDED_FILES_ARGS} -exec basename {} \;
     exit 0
 }
 
@@ -127,35 +138,32 @@ copy_all_files () {
     echo "[INFO] Ensuring ${OLD_FILE_DIR} exists and creating if it does not"
     create_old_file_dir
     echo "[INFO] Copying all dot files"
-    
+
     # Find the dot files in any subdirectory
-    find ${DIR} -type d -not -path "*/.git/*" -not -name ".git" -not -name "docs" | while IFS= read -r SUBDIR; do
-        echo "[INFO] Subdirectory: ${SUBDIR}"
-        find "${SUBDIR}" -type f -name ".*" -exec basename {} \; | while IFS= read -r FILE; do
-            if [ "${FILE}" != ".gitignore" ]; then
-                echo "[INFO] Moving existing ${FILE} from ${HOME} to ${OLD_FILE_DIR} if it exists"
-                if [ "${DRY_RUN}" = true ]; then
-                    echo "mv ${HOME}/${FILE} ${OLD_FILE_DIR}"
-                else
-                    if [ ! -L "${HOME}/${FILE}" ]; then
-                        mv ${HOME}/"${FILE}" "${OLD_FILE_DIR}"
-                    else
-                        echo "[INFO] Skipping symlink: ${HOME}/${FILE}"
-                    fi
-                fi
-                echo "[INFO] Creating symlink from ${SUBDIR}/${FILE} to $(readlink -f ${HOME}/${FILE})"
-                if [ "${DRY_RUN}" = true ]; then
-                    echo "ln -sf ${SUBDIR}/${FILE} ${HOME}/${FILE}"
-                else
-                    ln -sf "${SUBDIR}"/"${FILE}" ${HOME}/"${FILE}"
-                fi
+    find "${DIR}" -type f -name ".*" ${EXCLUDED_FILES_ARGS} -not -path "*/.git/*" | while IFS= read -r FILE; do
+        FILENAME=$(basename "${FILE}")
+        echo "[INFO] Moving existing ${FILENAME} from ${HOME} to ${OLD_FILE_DIR} if it exists"
+        if [ "${DRY_RUN}" = true ]; then
+            echo "mv ${HOME}/${FILENAME} ${OLD_FILE_DIR}"
+        else
+            if [ ! -L "${HOME}/${FILENAME}" ]; then
+                mv "${HOME}/${FILENAME}" "${OLD_FILE_DIR}"
+            else
+                echo "[INFO] Skipping symlink: ${HOME}/${FILENAME}"
             fi
-        done
+        fi
+        echo "[INFO] Creating symlink from ${FILE} to ${HOME}/${FILENAME}"
+        if [ "${DRY_RUN}" = true ]; then
+            echo "ln -sf ${FILE} ${HOME}/${FILENAME}"
+        else
+            ln -sf "${FILE}" "${HOME}/${FILENAME}"
+        fi
     done
+    # echo "[DEBUG] Linking directories"
     # If there are one or more subdirectories of ./Shell (relative to the current directory of this script) then create directory links to them in ${HOME}
     if [ -d "${DIR}/Shell" ]; then
         echo "[INFO] Creating directory links to ${DIR}/Shell subdirectories in ${HOME}"
-        find "${DIR}/Shell" -type d -not -path "*/.git/*" -not -name ".git" -not -name "docs" | while IFS= read -r SUBDIR; do
+        find "${DIR}/Shell" -mindepth 1 -maxdepth 1 -type d ${EXCLUDED_DIRS_ARGS} | while IFS= read -r SUBDIR; do
             echo "[INFO] Creating symlink from ${SUBDIR} to ${HOME}/$(basename ${SUBDIR})"
             if [ "${DRY_RUN}" = true ]; then
                 echo "ln -sf ${SUBDIR} ${HOME}/$(basename ${SUBDIR})"
@@ -167,62 +175,48 @@ copy_all_files () {
 }
 
 copy_selected_file () {
+    # Handle the file name passed in and if not file name is passed return an error
+    if [ -z "${COPY_FILE}" ]; then
+        echo "[ERROR] No file name passed in, exiting"
+        echo "[INFO] Use -l to list available files"
+        exit 1
+    fi
     echo "[INFO] Ensuring ${OLD_FILE_DIR} exists and creating if it does not"
     create_old_file_dir
     echo "[INFO] Copying ${COPY_FILE}"
-
-    # Find the file(s) in any subdirectory
-    find ${DIR} -type d -not -path "*/.git/*" -not -name ".git" -not -name "docs" | while IFS= read -r SUBDIR; do
-        echo "[INFO] Subdirectory: ${SUBDIR}"
-        find "${SUBDIR}" -type f -name "${COPY_FILE}" -exec basename {} \; | while IFS= read -r FILE; do
-            echo "[INFO] Moving existing ${FILE} from ${HOME} to ${OLD_FILE_DIR} if it exists"
-            if [ "${DRY_RUN}" = true ]; then
-                echo "mv ${HOME}/${FILE} ${OLD_FILE_DIR}"
-            else
-                if [ ! -L "${HOME}/${FILE}" ]; then
-                    mv ${HOME}/"${FILE}" "${OLD_FILE_DIR}"
-                else
-                    echo "[INFO] Skipping symlink: ${HOME}/${FILE}"
-                fi
-            fi
-            echo "[INFO] Creating symlink from ${SUBDIR}/${FILE} to $(readlink -f ${HOME}/${FILE})"
-            if [ "${DRY_RUN}" = true ]; then
-                echo "ln -sf ${SUBDIR}/${FILE} ${HOME}/${FILE}"
-            else
-                ln -sf "${SUBDIR}"/"${FILE}" ${HOME}/"${FILE}"
-            fi
-        done
-    done
-    # If the file being copied is .applets then also look for a ./Shell/applets directory in the repo relative to this script and create a symlink to it in ${HOME}
-    if [ "${COPY_FILE}" = ".applets" ]; then
-        if [ -d "${DIR}/Shell/applets" ]; then
-            echo "[INFO] Creating symlink from ${DIR}/Shell/applets to ${HOME}/applets"
-            if [ "${DRY_RUN}" = true ]; then
-                echo "ln -sf ${DIR}/Shell/applets ${HOME}/applets"
-            else
-                ln -sf "${DIR}/Shell/applets" ${HOME}/applets
-            fi
+    # Search the parent directory of this script and all subdirectories for the file name passed in
+    FILE=$(find "${DIR}" -type f -name "${COPY_FILE}" ${EXCLUDED_FILES_ARGS} ${EXCLUDED_DIRS_ARGS})
+    if [ -z "${FILE}" ]; then
+        echo "[ERROR] File ${COPY_FILE} not found"
+        exit 1
+    fi
+    # If the file is found then move the existing file to the OLD_FILE_DIR and create a symlink to the new file
+    FILENAME=$(basename "${FILE}")
+    echo "[INFO] Moving existing ${FILENAME} from ${HOME} to ${OLD_FILE_DIR} if it exists"
+    if [ "${DRY_RUN}" = true ]; then
+        echo "mv ${HOME}/${FILENAME} ${OLD_FILE_DIR}"
+    else
+        if [ ! -L "${HOME}/${FILENAME}" ]; then
+            mv "${HOME}/${FILENAME}" "${OLD_FILE_DIR}"
+        else
+            echo "[INFO] Skipping symlink: ${HOME}/${FILENAME}"
         fi
     fi
-}
-
-copy_theme_files () {
-    echo "[INFO] Copying theme files"
-    # ln -s ${DIR}/gw-agnoster.zsh-theme ${HOME}/.oh-my-zsh/themes/gw-agnoster.zsh-theme
-    for FILE in $(find ${DIR} -type f -name "*.zsh-theme" -exec basename {} \;); do
-        echo "[INFO] Moving existing dotfile from ${HOME} to ${OLD_FILE_DIR}"
+    echo "[INFO] Creating symlink from ${FILE} to ${HOME}/${FILENAME}"
+    if [ "${DRY_RUN}" = true ]; then
+        echo "ln -sf ${FILE} ${HOME}/${FILENAME}"
+    else
+        ln -sf "${FILE}" "${HOME}/${FILENAME}"
+    fi
+    # If COPY_FILE is .applets then we also need to symlink ${DIR}/Shell/applets to ${HOME}/.applets
+    if [ "${COPY_FILE}" = ".applets" ]; then
+        echo "[INFO] Creating symlink from ${DIR}/Shell/applets to ${HOME}/applets"
         if [ "${DRY_RUN}" = true ]; then
-            echo "mv ${HOME}/${FILE} ${OLD_FILE_DIR}"
+            echo "ln -sf ${DIR}/Shell/applets ${HOME}/applets"
         else
-            mv ${HOME}/"${FILE}" "${OLD_FILE_DIR}"
+            ln -sf "${DIR}/Shell/applets" "${HOME}/applets"
         fi
-        echo "[INFO] Creating symlink to ${FILE} in home directory."
-        if [ "${DRY_RUN}" = true ]; then
-            echo "ln -sf ${DIR}/${FILE} ${HOME}/${FILE}"
-        else
-            ln -sf "${DIR}"/"${FILE}" ${HOME}/"${FILE}"
-        fi
-    done
+    fi
 }
 
 main "${@}"
